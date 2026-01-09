620304b18e0ae4adcccdd73606ecfdfb23d6657e
Mikita Holub <mikita.holub.str@gmail.com>
2026-01-09 17:11:02 +0100

    just for streak

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+        {/* I'm got gona lose my strick*/}
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
