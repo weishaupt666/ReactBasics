3f84ce834298765be0fe865e8cdde45da4f68c03
Mikita Holub <mikita.holub.str@gmail.com>
2026-01-11 14:42:36 +0100

    Refactor: added props to components

--- react-basics/src/components/Todo.jsx
+++ react-basics/src/components/Todo.jsx
@@ -4,13 +4,21 @@ import TodoInfo from "./Todoinfo"
 import TodoList from "./TodoList"
 
 const Todo = () => {
+  const tasks = [
+    {id: 'task-1', title: 'Buy milk', isDone: false},
+    {id: 'task-2', title: 'Pet the cat', isDone: true},
+  ]
+
     return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
       <AddTaskForm />
       <SearchTaskForm />
-      <TodoInfo />
-      <TodoList />
+      <TodoInfo
+        total={tasks.length}
+        done={tasks.filter(({isDone}) => isDone).length}
+      />
+      <TodoList tasks={tasks}/>
     </div>
     )
 }
