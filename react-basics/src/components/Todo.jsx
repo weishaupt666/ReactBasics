dbb614b9a920b1b524f38d24427b896039fed1c6
Mikita Holub <mikita.holub.str@gmail.com>
2026-01-12 17:48:09 +0100

    Feat: added events for page alements

--- react-basics/src/components/Todo.jsx
+++ react-basics/src/components/Todo.jsx
@@ -9,16 +9,45 @@ const Todo = () => {
     {id: 'task-2', title: 'Pet the cat', isDone: true},
   ]
 
+  const deleteAllTasks = () => {
+    console.log('Delete all tasks')
+  }
+
+  const deleteTask = (taskId) => {
+    console.log('Delete Id task')
+  }
+
+  const toggleTaskComplete = (taskId, isDone) => {
+    console.log(`Task ${taskId} ${isDone ? 'done' : 'not done'}`)
+  }
+
+  const filterTasks = (query) => {
+    console.log(`Search ${query}`)
+  }
+
+  const addTask = () => {
+    console.log('Task added')
+  }
+
     return (
     <div className="todo">
       <h1 className="todo__title">To Do List</h1>
-      <AddTaskForm />
-      <SearchTaskForm />
+      <AddTaskForm 
+        addTask={addTask}
+      />
+      <SearchTaskForm 
+        onSearchInput={filterTasks}
+      />
       <TodoInfo
         total={tasks.length}
         done={tasks.filter(({isDone}) => isDone).length}
+        onDeleteAllButtonClick={deleteAllTasks}
       />
-      <TodoList tasks={tasks}/>
+      <TodoList 
+        tasks={tasks}
+        onDeleteTaskButtonClick={deleteTask}
+        onTaskCompleteChange={toggleTaskComplete}
+      />
     </div>
     )
 }
